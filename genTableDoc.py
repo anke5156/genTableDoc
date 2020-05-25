e2b45a9d4c8b013376feef8fc9a228186436f422
anke5156 <anke.wang@foxmail.com>
2020-05-25 22:03:53 +0800

    代码优化

--- genTableDoc.py
+++ genTableDoc.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-from datetime import time, datetime
-
+from datetime import datetime
 from pydbclib import connect as con
 import xlwt
 
@@ -19,10 +18,12 @@ class GenTableDoc(object):
     def __init__(self, connect, database):
         self.db = con(f"mysql+pymysql://{connect}")
         self.database = database
-        self.tabs = []
-        self.tab_en = ''
-        self.tab_cn = ''
+        self.tbls = {}
+        self.tbl_en = ''
+        self.tbl_cn = ''
         self.sheetName = ''
+        self.title_table = ['数据库', '表名', '表注释', '类型', '数据量', '创建时间', '更新时间', '表结构']
+        self.title_column = ['表名', '字段序号', '字段', '类型', '主键', '字段描述']
 
     def _sqls(self, id):
         """
@@ -49,7 +50,7 @@ class GenTableDoc(object):
                     left join information_schema.tables b on a.table_schema=b.table_schema and a.table_name=b.table_name
                     where 1=1
                     and b.table_schema = '{self.database}' -- 表所在数据库
-                    and b.table_name = '{self.tab_en}' -- 你要查的表
+                    and b.table_name = '{self.tbl_en}' -- 你要查的表
                     order by a.table_schema,a.table_name,a.ordinal_position;
                     """
 
@@ -65,7 +66,6 @@ class GenTableDoc(object):
 
         # 调整样式
         style = xlwt.XFStyle()
-
         # 边框
         borders = xlwt.Borders()  # Create Borders
         borders.left = xlwt.Borders.THIN
@@ -73,14 +73,13 @@ class GenTableDoc(object):
         borders.top = xlwt.Borders.THIN
         borders.bottom = xlwt.Borders.THIN
         style.borders = borders
-
+        # 对其方式
         alignment1 = xlwt.Alignment()
         alignment2 = xlwt.Alignment()
         # 0x01(左端对齐)、0x02(水平方向上居中对齐)、0x03(右端对齐)
         alignment1.horz = 0x02
         # 0x00(上端对齐)、 0x01(垂直方向上居中对齐)、0x02(底端对齐)
         alignment1.vert = 0x01
-
         # 字体
         fontT = xlwt.Font()
         fontF = xlwt.Font()
@@ -91,7 +90,7 @@ class GenTableDoc(object):
         # 表列表
         for i, lin in enumerate(data):
             self.sheetName = f'tbl{i}'
-            # 第一列填序号
+            # 第一列填充序号
             if i < len(data) - 1:
                 sheet.write(i + 1, 0, i + 1, style)  # 序号
             for j, cell in enumerate(lin):
@@ -99,6 +98,7 @@ class GenTableDoc(object):
                 if isinstance(cell, datetime):
                     style.num_format_str = 'YYYY/M/D h:mm:ss'
                 elif i == 0:
+                    # 处理表头
                     style.font = fontT
                     style.alignment = alignment1
                 sheet.write(i, j + 1, cell, style)
@@ -107,26 +107,25 @@ class GenTableDoc(object):
                 style.num_format_str = ''
                 style.font = fontF
                 style.alignment = alignment2
+
+            # 添加表结构超链接
             if i > 0:
                 link = f'HYPERLINK("#{self.sheetName}!B1";"查看")'
                 sheet.write(i, len(lin) + 1, xlwt.Formula(link), style)
 
         # 表结构
-        for i, tb in enumerate(self.tabs):
-            for self.tab_en, self.tab_cn in tb.items():
-                pass
+        for i, (self.tbl_en, self.tbl_cn) in enumerate(self.tbls.items()):
             self.sheetName = f'tbl{i + 1}'
             db_read = self.db.read(self._sqls(1), as_dict=False)
             sheet = writebook.add_sheet(self.sheetName)
 
-            sheet.write_merge(0, 0, 0, 5, self.tab_en)
-            sheet.write_merge(1, 1, 0, 5, self.tab_cn)  # 开始行,结束行, 开始列, 结束列
+            sheet.write_merge(0, 0, 0, 5, self.tbl_en)
+            sheet.write_merge(1, 1, 0, 5, self.tbl_cn)  # 开始行,结束行, 开始列, 结束列
             link = 'HYPERLINK("#表列表!B3";"返回")'
             sheet.write_merge(2, 2, 0, 5, xlwt.Formula(link))
 
-            title = ['表名', '字段序号', '字段', '类型', '主键', '字段描述']
             # 表头
-            for j, cell in enumerate(title):
+            for j, cell in enumerate(self.title_column):
                 style.font = fontT
                 style.alignment = alignment1
                 sheet.write(4, j, cell, style)
@@ -147,14 +146,15 @@ class GenTableDoc(object):
         查询表列表
         :return:
         """
-        data = [['数据库', '表名', '表注释', '类型', '数据量', '创建时间', '更新时间', '表结构']]
-        read = self.db.read(self._sqls(0), as_dict=False)
-        for lin in read.get_all():
-            self.tabs.append({lin[1]: lin[2]})
+        data = []
+        data.append(self.title_table)
+        lins = self.db.read(self._sqls(0), as_dict=False)
+        for lin in lins.get_all():
+            self.tbls[lin[1]] = lin[2]
             data.append(lin)
-        print(self.tabs)
-        self._writeExcel('tableSche.xlsx', data)
+        print(self.tbls)
+        self._writeExcel(f'tableSche_{self.database}.xlsx', data)
 
 
 if __name__ == '__main__':
-    GenTableDoc(connect='root:123456@127.0.0.1:3306', database='mysql').start()
+    GenTableDoc(connect='root:123456@127.0.0.1:3306', database='dangan').start()
